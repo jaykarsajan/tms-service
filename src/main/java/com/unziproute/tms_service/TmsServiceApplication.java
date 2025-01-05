2cfa9d58bf2c291767aa5e6484b9a9931b87e515
Sajan <jaykarsajan@gmail.com>
2025-01-05 14:06:28 +0530

    Initial commit

---
+++ src/main/java/com/unziproute/tms_service/TmsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.unziproute.tms_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unziproute/tms_service/TmsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unziproute.tms_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
