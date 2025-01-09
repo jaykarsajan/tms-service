0a297d939c9d170be8a58f7e30f2f702c989ea96
Sajan <jaykarsajan@gmail.com>
2025-01-09 19:43:48 +0530

    constants commit

---
+++ src/main/java/com/unziproute/tms_service/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.unziproute.tms_service.utils;
+
+public class Constants {
+
+    public static final String pending = "PENDING";
+    public static final String finished = "FINISHED";
+    public static final String deleted = "DELETED";
+
+    public Constants() {
+    }
+}
